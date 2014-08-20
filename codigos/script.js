f25a1bcde7cc83f93d45b9ed4f6c1d2ba1f2127f
Jairo Ramirez <jairo.r88@gmail.com>
2014-08-20 01:03:52 -0500

    aplicando buenas practicas
    Modifique los archivos index.html y script.js para quitar el atributo
    onclick y mejor asignar a los botones un escuchador de eventos con
    addEventListener. Tambien resumi un poco el uso de los condicionales

--- codigos/script.js
+++ codigos/script.js
@@ -1,7 +1,35 @@
+/**********
+	CREADO POR JAIRO RAMIREZ
+	EJERCICIO NUM 1 PROGRAMACION  BASICA
+	                         *********/
+
 //variables necesarias
+var botonpiedra;
+var botonpapel;
+var botontijera;
+var botonlagarto;
+var botonspock;
 var jugadas= ["Piedra","Papel","Tijeras","Lagarto","Spock"];
 var jugadausuario;
 var jugadamaquina;
+
+//funcion inicio para que primero cargue todo el html y asi poder tomar los elementos
+function inicio()
+{
+	//Aqui tomo los botones y los asigno a las variables
+	botonpiedra= document.getElementById("btnpiedra");
+	botonpapel= document.getElementById("btnpapel");
+	botontijera= document.getElementById("btntijera");
+	botonlagarto= document.getElementById("btnlagarto");
+	botonspock= document.getElementById("btnspock");
+
+	//Aqui le añado un escuchador del evento clic a los botones
+	botonpiedra.addEventListener("click",eligio_piedra);
+	botonpapel.addEventListener("click",eligio_papel);
+	botontijera.addEventListener("click",eligio_tijeras);
+	botonlagarto.addEventListener("click",eligio_lagarto);
+	botonspock.addEventListener("click",eligio_spock);
+}
 
 //función que genera numero aleatorio de 0 a 4
 function numaleatorio()
@@ -41,6 +69,7 @@ function eligio_spock()
   resultado();
 }
 
+
 //función para mostrar cual es el ganador
 function resultado()
 {
@@ -51,94 +80,63 @@ function resultado()
 		if(jugadamaquina=="Piedra")
 		{
 			document.getElementById('ganador').innerHTML="Empate! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Papel")
+		}else if(jugadamaquina=="Papel"||jugadamaquina=="Spock")
 		{
 			document.getElementById('ganador').innerHTML="Perdiste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Tijeras")
+		}else if(jugadamaquina=="Tijeras"||jugadamaquina=="Lagarto")
 		{
 			document.getElementById('ganador').innerHTML="Ganaste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Lagarto")
-		{
-			document.getElementById('ganador').innerHTML="Ganaste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Spock")
-		{
-			document.getElementById('ganador').innerHTML="Perdiste! La maquina eligió "+jugadamaquina;
 		}
 	}
-
 	else
 	if(jugadausuario=="Papel")
 	{
-		if(jugadamaquina=="Piedra")
+		if(jugadamaquina=="Piedra"||jugadamaquina=="Spock")
 		{
 			document.getElementById('ganador').innerHTML="Ganaste! La maquina eligió "+jugadamaquina;
 		}else if(jugadamaquina=="Papel")
 		{
 			document.getElementById('ganador').innerHTML="Empate! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Tijeras")
+		}else if(jugadamaquina=="Tijeras"||jugadamaquina=="Lagarto")
 		{
 			document.getElementById('ganador').innerHTML="Perdiste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Lagarto")
-		{
-			document.getElementById('ganador').innerHTML="Perdiste! La maquina eligió"+jugadamaquina;
-		}else if(jugadamaquina=="Spock")
-		{
-			document.getElementById('ganador').innerHTML="Ganaste! La maquina eligió "+jugadamaquina;
 		}
 	}
 	else
 	if(jugadausuario=="Tijeras")
 	{
-		if(jugadamaquina=="Piedra")
+		if(jugadamaquina=="Piedra"||jugadamaquina=="Spock")
 		{
 			document.getElementById('ganador').innerHTML="Perdiste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Papel")
+		}else if(jugadamaquina=="Papel"||jugadamaquina=="Lagarto")
 		{
 			document.getElementById('ganador').innerHTML="Ganaste! La maquina eligió "+jugadamaquina;
 		}else if(jugadamaquina=="Tijeras")
 		{
 			document.getElementById('ganador').innerHTML="Empate! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Lagarto")
-		{
-			document.getElementById('ganador').innerHTML="Ganaste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Spock")
-		{
-			document.getElementById('ganador').innerHTML="Perdiste! La maquina eligió "+jugadamaquina;
 		}
 	}
 	else
 	if(jugadausuario=="Lagarto")
 	{
-		if(jugadamaquina=="Piedra")
+		if(jugadamaquina=="Piedra"||jugadamaquina=="Tijeras")
 		{
 			document.getElementById('ganador').innerHTML="Perdiste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Papel")
+		}else if(jugadamaquina=="Papel"||jugadamaquina=="Spock")
 		{
 			document.getElementById('ganador').innerHTML="Ganaste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Tijeras")
-		{
-			document.getElementById('ganador').innerHTML="Perdiste! La maquina eligió "+jugadamaquina;
 		}else if(jugadamaquina=="Lagarto")
 		{
 			document.getElementById('ganador').innerHTML="Empate! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Spock")
-		{
-			document.getElementById('ganador').innerHTML="Ganaste! La maquina eligió "+jugadamaquina;
 		}
 	}
 	else
 	if(jugadausuario=="Spock")
 	{
-		if(jugadamaquina=="Piedra")
+		if(jugadamaquina=="Piedra"||jugadamaquina=="Tijeras")
 		{
 			document.getElementById('ganador').innerHTML="Ganaste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Papel")
-		{
-			document.getElementById('ganador').innerHTML="Perdiste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Tijeras")
-		{
-			document.getElementById('ganador').innerHTML="Ganaste! La maquina eligió "+jugadamaquina;
-		}else if(jugadamaquina=="Lagarto")
+		}else if(jugadamaquina=="Papel"||jugadamaquina=="Lagarto")
 		{
 			document.getElementById('ganador').innerHTML="Perdiste! La maquina eligió "+jugadamaquina;
 		}else if(jugadamaquina=="Spock")
